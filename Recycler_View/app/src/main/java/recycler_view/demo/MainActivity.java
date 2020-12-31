1f290ea16dbfe39d18fab402f65f4eb3b72253fc
Michael <camposm1205@gmail.com>
2020-12-31 00:07:27 -0500

    Implemented method for providing data to fill RecyclerView

--- Recycler_View/app/src/main/java/recycler_view/demo/MainActivity.java
+++ Recycler_View/app/src/main/java/recycler_view/demo/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public List<String> loadData() {
+        List<String> list = new LinkedList<>();
+        list.add("Stony Brook University");
+        list.add("Suffolk County Community College");
+        list.add("Farmingdale State College");
+        list.add("University at Albany");
+        list.add("University of Buffalo");
+        list.add("Queen's College");
+        list.add("Hunter's College");
+        list.add("St. Joesph's College");
+        list.add("Long Island University");
+        return list;
     }
 }
